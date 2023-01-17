16652f01bf712654547b38fd84f8fc9c1e69e606
And-Bond <aloxnesir@gmail.com>
2023-01-17 18:51:02 +0200

    finishing all contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { ContactForm } from "./contact-form/contact-form.component";
-import { ContactList } from "./contact-list/contact-list.component";
-import { FilterComponent } from "./filter/filter.component";
+import { AppComponent }   from './app.component';
+import {ContactForm} from './contact-form/contact-form.component'
+import {ContactList} from './contact-list/contact-list.component'
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ContactForm, ContactList, FilterComponent],
-  bootstrap: [AppComponent],
+    imports:      [ BrowserModule, FormsModule ],
+    declarations: [ AppComponent,ContactForm,ContactList ],
+    bootstrap:    [ AppComponent ]
 })
-export class AppModule {}
+
+export class AppModule { }
